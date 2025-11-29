370ed65ca0a4613defd3a64540eb47751b728d84
xcastillo25 <xcastillo25@gmail.com>
2025-11-28 23:21:43 -0600

    upd

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 //export const API_URL = 'https://tonygames25-002-site1.htempurl.com/api';
 
-export const API_URL = 'http://localhost:8020/api';
+export const API_URL = 'http://tonygames25-001-site11.stempurl.com/api';
